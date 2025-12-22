3eb29d4319eb3ff5b652727204074f146e01c76c
Frédéric Poliquin <fpoliquin@bgy.ca>
2025-12-22 13:54:42 -0500

    #9.2

--- src/main/java/advent2025/no9/TileFloor.java
+++ src/main/java/advent2025/no9/TileFloor.java
@@ -1,15 +1,17 @@
 package advent2025.no9;
 
-import advent2025.no8.Playground;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class TileFloor {
     private final List<Point> redPoints;
+    private final Set<Point> redOrGreenPoints = new HashSet<>();
+    private final Set<Edge> edges = new HashSet<>();
+    private final Map<Point, Boolean> insideCache = new HashMap<>();
+    private boolean linesArePainted = false;
+    private boolean edgesAreDone = false;
 
     public TileFloor(String data) {
         redPoints = data.lines()
@@ -23,6 +25,14 @@ public class TileFloor {
     }
 
     public long computeLargestRedArea() {
+        return computeLargestRedArea(false);
+    }
+
+    public long computeLargestRedAreaWithinRedOrGreenTiles() {
+        return computeLargestRedArea(true);
+    }
+
+    private long computeLargestRedArea(boolean inside) {
         if (redPoints.size() <= 1) {
             return 1;
         }
@@ -34,12 +44,152 @@ public class TileFloor {
                 var area = redPoints.get(i).computeArea(redPoints.get(j));
 
                 if (area > maxArea) {
-                    maxArea = area;
+                    if (inside) {
+                        if (rectangleIsAllWithinEdges(redPoints.get(i), redPoints.get(j))) {
+                            maxArea = area;
+                        }
+                    } else {
+                        maxArea = area;
+                    }
                 }
             }
         }
 
         return maxArea;
+    }
+
+    private boolean rectangleIsAllWithinEdges(Point p1, Point p2) {
+        var minX = Math.min(p1.x, p2.x);
+        var maxX = Math.max(p1.x, p2.x);
+        var minY = Math.min(p1.y, p2.y);
+        var maxY = Math.max(p1.y, p2.y);
+
+        return horizontalLineIsWithinEdges(minX, maxX, minY)
+                && horizontalLineIsWithinEdges(minX, maxX, maxY)
+                && verticalLineIsWithinEdges(minX, minY, maxY)
+                && verticalLineIsWithinEdges(maxX, minY, maxY);
+    }
+
+    private boolean horizontalLineIsWithinEdges(int minX, int maxX, int y) {
+        for (int i=minX; i <= maxX; ++i) {
+            if (!isRedOrGreen(i, y)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean verticalLineIsWithinEdges(int x, int minY, int maxY) {
+        for (int i=minY; i <= maxY; ++i) {
+            if (!isRedOrGreen(x, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isRedOrGreen(int x, int y) {
+        paintLines();
+        Point point = new Point(x, y);
+
+        var cached = insideCache.get(point);
+
+        if (cached != null) {
+            return cached;
+        }
+
+        var isOnEdge = redOrGreenPoints.contains(point);
+
+        if (isOnEdge) {
+            return true;
+        }
+
+        var res = isWithinEdges(point);
+
+        insideCache.put(point, res);
+
+        return res;
+    }
+
+    private boolean isWithinEdges(Point point) {
+        computeEdges();
+
+        var count = 0;
+        for(Edge edge : edges) {
+            if (edge.crossesRay(point)) {
+                ++count;
+            }
+        }
+
+        return isOdd(count);
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) != 0;
+    }
+
+    private void computeEdges() {
+        if (edgesAreDone) {
+            return;
+        }
+
+        Point lastPoint = null;
+
+        for (Point p : redPoints) {
+            if (lastPoint != null) {
+                edges.add(new Edge(lastPoint, p));
+            }
+
+            lastPoint = p;
+        }
+
+        edges.add(new Edge(lastPoint, redPoints.getFirst()));
+
+        edgesAreDone = true;
+    }
+
+    private void paintLines() {
+        if (linesArePainted) {
+            return;
+        }
+
+        Point lastPoint = null;
+
+        for (var point : redPoints) {
+            redOrGreenPoints.add(point);
+
+            if (lastPoint != null) {
+                paintLine(point, lastPoint);
+            }
+
+            lastPoint = point;
+        }
+
+        if (lastPoint != null) {
+            paintLine(redPoints.getFirst(), lastPoint);
+        }
+
+        linesArePainted = true;
+    }
+
+    private void paintLine(Point p1, Point p2) {
+        if (p1.x == p2.x) {
+            var minY = Math.min(p1.y, p2.y);
+            var maxY = Math.max(p1.y, p2.y);
+
+            for (int i=minY+1; i < maxY; ++i) {
+                redOrGreenPoints.add(new Point(p1.x, i));
+            }
+        } else if (p1.y == p2.y) {
+            var minX = Math.min(p1.x, p2.x);
+            var maxX = Math.max(p1.x, p2.x);
+
+            for (int i=minX+1; i < maxX; ++i) {
+                redOrGreenPoints.add(new Point(i, p1.y));
+            }
+        } else {
+            throw new RuntimeException("Points non in line: " + p1 + " " + p2);
+        }
     }
 
     private record Point(int x, int y) {
@@ -51,6 +201,24 @@ public class TileFloor {
         }
     }
 
+    private record Edge(Point p1, Point p2) {
+
+        public boolean crossesRay(Point point) {
+            if (p1.x != p2.x) {
+                return false;
+            }
+
+            return point.x < p1.x && isWithinY(point.y);
+        }
+
+        private boolean isWithinY(int y) {
+            var minY = Math.min(p1.y, p2.y);
+            var maxY = Math.max(p1.y, p2.y);
+
+            return y > minY && y <= maxY;
+        }
+    }
+
     public static void main(String... args) throws Exception {
 
         var data = Files.readString(
@@ -59,7 +227,9 @@ public class TileFloor {
         var floor = new TileFloor(data);
 
         var res = floor.computeLargestRedArea();
-
         System.out.println("Largest area: " + res);
+
+        var res2 = floor.computeLargestRedAreaWithinRedOrGreenTiles();
+        System.out.println("Largest area (green or red): " + res2);
     }
 }
--- src/test/java/advent2025/no9/TileFloorTest.java
+++ src/test/java/advent2025/no9/TileFloorTest.java
@@ -74,4 +74,61 @@ public class TileFloorTest {
 
         assertThat(res).isEqualTo(50);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,0,true",
+            "5,0,true",
+            "10,0,true",
+            "11,0,false",
+            "10,4,true",
+            "10,8,true",
+            "10,9,false",
+            "0,8,true",
+            "0,4,true",
+            "0,9,false"
+    })
+    public void ShouldFindPointOnRedAndGreenLines(int x, int y, boolean expected) {
+        var floor = new TileFloor("0,0\n10,0\n10,8\n0,8");
+
+        var res = floor.isRedOrGreen(x, y);
+
+        assertThat(res).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,true",
+            "9,1,true",
+            "9,7,true",
+            "1,7,true",
+            "0,11,false",
+            "10,9,false",
+            "0,9,false"
+    })
+    public void ShouldFindPointInsideRedAndGreenLines(int x, int y, boolean expected) {
+        var floor = new TileFloor("0,0\n10,0\n10,8\n0,8");
+
+        var res = floor.isRedOrGreen(x, y);
+
+        assertThat(res).isEqualTo(expected);
+    }
+
+    @Test
+    public void ShouldComputeLargestAreaWithinRedOrGreenTilesWithoutObstacle() {
+        var floor = new TileFloor("0,0\n10,0\n10,8\n0,8");
+
+        var res = floor.computeLargestRedAreaWithinRedOrGreenTiles();
+
+        assertThat(res).isEqualTo(99);
+    }
+
+    @Test
+    public void ShouldComputeLargestAreaWithinRedOrGreenTiles() {
+        var floor = new TileFloor(SAMPLE);
+
+        var res = floor.computeLargestRedAreaWithinRedOrGreenTiles();
+
+        assertThat(res).isEqualTo(24);
+    }
 }
